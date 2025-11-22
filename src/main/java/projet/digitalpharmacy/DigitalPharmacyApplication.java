3b1dcefe0104be40a1be76252483127e6e3256ed
kuyf <mmhimer0@gmail.com>
2025-11-22 14:56:17 +0100

    empty with dependencies

---
+++ src/main/java/projet/digitalpharmacy/DigitalPharmacyApplication.java
@@ -0,0 +1,13 @@
+package projet.digitalpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalPharmacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projet/digitalpharmacy/DigitalPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.digitalpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
